db83cb0e7c283c0895cc4f554bcd5576dff0b2eb
Dương Đỗ <dustin.do95@gmail.com>
2019-06-24 14:41:49 +0700

    chore: remove mobile package, it should be moved to template repo

--- packages/mobile/src/hooks/useTheme.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react'
-import { ThemeContext } from 'styled-components'
-import { Theme } from 'theme'
-
-const useTheme = (): Theme => {
-  return useContext(ThemeContext)
-}
-
-export default useTheme
