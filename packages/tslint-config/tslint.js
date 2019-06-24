52eb725f9c6ad1e49898538fdbd9977949352133
Dương Đỗ <dustin.do95@gmail.com>
2019-06-24 12:36:46 +0700

    feat(tslint-config): update config

--- packages/tslint-config/tslint.js
+++ packages/tslint-config/tslint.js
@@ -1,11 +1,29 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   rulesDirectory: [
     path.join(path.dirname(require.resolve('tslint-config-airbnb')), './'),
+    path.join(path.dirname(require.resolve('tslint-config-prettier')), './'),
   ],
   rules: {
     semicolon: false,
     'trailing-comma': false,
-  }
+    eofline: false,
+    'no-consecutive-blank-lines': false,
+    quotemark: {
+      severity: 'warn',
+      options: ['single', 'jsx-double'],
+    },
+    'no-trailing-whitespace': false,
+    'ordered-imports': {
+      severity: 'warn',
+    },
+    'import-name': false,
+    align: false,
+    'object-curly-spacing': {
+      severity: 'warn',
+    },
+    'ter-arrow-parens': [true, 'always'],
+    'max-line-length': false,
+  },
 }
