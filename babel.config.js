64350c7db80f9395c290ba8fd99b5750d284b18d
Dương Đỗ <dustin.do95@gmail.com>
2019-08-22 16:52:58 +0700

    chore: add dev env

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+export default function(api) {
+  api.cache(true)
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+        },
+      ],
+    ],
+  }
+}
